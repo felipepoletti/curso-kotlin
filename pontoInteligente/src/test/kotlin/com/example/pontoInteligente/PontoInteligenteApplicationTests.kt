bb4e52041eb34edb07ecbeed6441b4b1117cedb0
= <felipepolettig@gmail.com>
2023-09-12 18:53:39 -0300

    first commit

---
+++ pontoInteligente/src/test/kotlin/com/example/pontoInteligente/PontoInteligenteApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.pontoInteligente
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
